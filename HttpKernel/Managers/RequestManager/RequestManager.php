3f5bc229a9c4afbb1a4690180c4be3d749395fbe
milinsky <mail@milinsky.ru>
2019-01-12 16:33:29 +1000

    RequestManager

--- HttpKernel/Managers/RequestManager/RequestManager.php
+++ HttpKernel/Managers/RequestManager/RequestManager.php
@@ -1,1 +1,30 @@
- 
+<?php
+
+declare(strict_types=1);
+
+namespace Marussia\HttpKernel\Managers\RequestManager;
+
+use Marussia\Components\Request\Request as Request;
+
+class
+{
+    private $eventBus;
+    
+    private $request;
+
+    public function __construct(Request $request EventManager $event_bus)
+    {
+        $this->eventBus = $event_bus;
+        
+        $this->request = $request;
+    }
+    
+    public function run()
+    {
+        if ($this->request->getContext() === 'Ajax') {
+            $this->eventBus->eventDispatch('Kernel.Request', 'AjaxRequestReady', $this->request);
+        } else {
+            $this->eventBus->eventDispatch('Kernel.Request', 'RequestReady', $this->request);
+        }
+    }
+}
