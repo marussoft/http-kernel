39fedcc152c17b36f04d2b278536a346e5740395
Михаил <mail@milinsky.ru>
2019-02-03 22:40:38 +1000

    Auth

--- src/Managers/Auth/Auth.php
+++ src/Managers/Auth/Auth.php
@@ -5,11 +5,26 @@ declare(strict_types=1);
 namespace Marussia\HttpKernel\Managers\Auth;
 
 use Marussia\HttpKernel\App as App;
+use Marussia\Authorization\Authorization as Authorization;
+use Marussia\DependencyInjection\Container as Container;
 
 class Auth
 {
     public function init($request)
     {
-        App::event('App.Auth', 'Ready');
+        $container = new Container;
+        
+        $auth = $container->instance(Authorization::class, [MASTER_KEY]);
+        
+        if ($auth->isAuth()) {
+            $data = $auth->getData();
+        } else {
+            $auth->setGuest();
+            $data['role'] = 'guest';
+            $data['uid'] = 0;
+            $data['gid'] = $auth->getGuestId();
+        }
+
+        App::event('App.Auth', 'Ready', $data);
     }
 }
